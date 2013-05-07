7f096e722db54fa2d1ac099f5cc3e9cd7032909c
Tan Nguyen <tdnguyen85@gmail.com>
2013-05-07 16:45:37 -0700

    Modified user model

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,5 @@
 class Bookmark < ActiveRecord::Base
-  attr_accessible :tag_list, :author_first_name, :author_last_name, :published_on, :publisher, :quote, :retrieved_on, :string, :title, :url
+  attr_accessible :user_id, :user_name, :tag_list, :author_first_name, :author_last_name, :published_on, :publisher, :quote, :retrieved_on, :string, :title, :url
   belongs_to :user
   acts_as_taggable
 
