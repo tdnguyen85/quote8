4667f26ed3238e1312d31cc7666dabec3fddc8eb
Tracy Weiss <tracyleeweiss@gmail.com>
2013-05-10 10:48:17 -0700

    added js shake action to logo

---
+++ app/views/layouts/mouse_over.js
@@ -0,0 +1,8 @@
+<div class="apps_bookmarklet_button" onmouseover="Element.show($('bookmarklet_arrow'))" onmouseout="Element.hide($('bookmarklet_arrow'))">
+<a href="javascript:var d=document,w=window,e=w.getSelection,k=d.getSelection,x=d.selection,s=(e?e():(k)?k():(x?x.createRange().text:0)),f='http://www.tumblr.com/share',l=d.location,e=encodeURIComponent,p='?v=3&u='+e(l.href) +'&t='+e(d.title) +'&s='+e(s),u=f+p;try{if(!/^(.*\.)?tumblr[^.]*$/.test(l.host))throw(0);tstbklt();}catch(z){a =function(){if(!w.open(u,'t','toolbar=0,resizable=0,status=1,width=450,height=430'))l.href=u;};if(/Firefox/.test(navigator.userAgent))setTimeout(a,0);else a();}void(0)
+" onclick="return explain_bookmarklet();" id="bookmarklet_button" class="chrome huge"  style="position:relative" title="Drag this to your Bookmarks Bar to install">Share on Tumblr</a>
+<div style="position:absolute; bottom:13px; left:200px; width:159px; height:207px; background:url('http://assets.tumblr.com/images/blue_arrow_small.png') top left no-repeat transparent; z-index:150; display:none" id="bookmarklet_arrow" onmouseover="Element.show($('bookmarklet_arrow'))" onmouseout="Element.hide($('bookmarklet_arrow'))"></div>
+</div>
+</div>
+
+</div>
