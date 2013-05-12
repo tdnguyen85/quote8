12912a8f2b0b49c9afad63c62d883438eb7d3ed7
Tracy Weiss <tracyleeweiss@gmail.com>
2013-05-12 11:37:48 -0700

    styled all popups, changed screenshot in landing page

--- app/assets/javascripts/animations.js
+++ app/assets/javascripts/animations.js
@@ -1,31 +1,24 @@
-$(document).ready(function() {
+// logo animation
 
+$(document).ready(function() {
   $("#shake").mouseenter(function () {
     $(this).effect("shake", { times:2, distance: 3}, 500);
   });
 
+//arrow appearance over bookmarklet
+
   $('#bookmarklet-container').hover(function(){
     if (!$('#bookmarklet-arrow').is(':visible')){
-     $('#bookmarklet-arrow').fadeIn('slow');
+      $('#bookmarklet-arrow').fadeIn('slow');
     }
-   }, function() {
+  }, function() {
     $('#bookmarklet-arrow').fadeOut('slow');
+  });
 
-   });
-
+//extend new quote fields
 
   $('#show_more').on('click', function(event){
     event.preventDefault();
-  $('#hidden').toggle('slow');
+    $('#hidden').toggle('slow');
   });
-
-  //   $('#bookmarklet-arrow').animate({
-  //     opacity: 1,    
-  //   }, 500);
-  // }, function() {
-  //   $('#bookmarklet-arrow').animate({
-  //     opacity: 0,
-  //     }  , 500);
-  // }); 
-
 });
