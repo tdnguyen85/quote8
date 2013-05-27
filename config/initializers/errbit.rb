fcfbc1960c2d48e2a5e563e73002452bba844390
Jonathan Lai <jonathanlai@eltux.net>
2013-05-27 12:02:05 -0700

    changed initializer

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,5 @@
 Airbrake.configure do |config|
-  config.api_key = 'ENV[ERRBIT]'
+  config.api_key = ENV['ERRBIT']
   config.host    = 'oopsy.herokuapp.com'
   config.port    = 80
   config.secure  = config.port == 443
